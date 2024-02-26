69f8b453c2d623baf51d724f013d9121d0d5dc37
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-02-26 14:42:28 +0100

    repro

--- Features.ts
+++ Features.ts
@@ -3,32 +3,37 @@
 module Mankala {
     export class Features {
 
-		turnContinues:boolean;
-		seedStoredCount:number;
-		capturedCount:number;
-		spaceCaptured:number;
+        turnContinues: boolean;
+        seedStoredCount: number;
+        capturedCount: number;
+        spaceCaptured: number;
 
-		constructor() {
-			this.turnContinues = false;
-			this.seedStoredCount = 0;
-			this.capturedCount = 0;
-			this.spaceCaptured = NoSpace;
-		}
-
-        clear() {
+        constructor() {
             this.turnContinues = false;
             this.seedStoredCount = 0;
             this.capturedCount = 0;
             this.spaceCaptured = NoSpace;
         }
 
-        toString() {
+        clear() {
+            this.turnContinues = false;
+            this.seedStoredCount = 0;
+            this.capturedCount = 0;
+            this.spaceCaptured = NoSpace;
+
+            const foo = true;
+            const bar = 33;
+
+            // this.toString()
+        }
+
+        toString(alles: boolean, klar: number) {
             var stringBuilder = '';
             if (this.turnContinues) {
                 stringBuilder += ' turn continues,';
             }
-            stringBuilder += (' stores '+ this.seedStoredCount);
-            if (this.capturedCount>0) {
+            stringBuilder += (' stores ' + this.seedStoredCount);
+            if (this.capturedCount > 0) {
                 stringBuilder += (' captures ' + this.capturedCount + ' from space ' + this.spaceCaptured);
             }
             return stringBuilder;
